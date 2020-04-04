0ca009ebf01ea75b392798c6a655e0d3744c2ae5
Adham Wael <adhamwaelmahmoudsalem@gmail.com>
2020-04-04 18:25:29 +0200

    Message

--- src/DashboardsPack/Client/TablesPane.java
+++ src/DashboardsPack/Client/TablesPane.java
@@ -1,6 +1,7 @@
 package DashboardsPack.Client;
 
 import MainPack.Restaurant;
+import ReservationPack.Table;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -8,37 +9,36 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 
+import javax.swing.*;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
-public class TablesPane implements Initializable {
+public class TablesPane implements Initializable{
 
     @FXML
-    JFXButton table1;
-    @FXML
-    JFXButton table2;
-    @FXML
-    JFXButton table3;
-    @FXML
-    JFXButton table4;
-    @FXML
-    JFXButton table5;
-    @FXML
-    JFXButton table6;
-    @FXML
-    JFXButton table7;
+    GridPane Gridpane = new GridPane();
+
+    List<JFXButton> tab = new ArrayList<>();
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        table1.setText(Restaurant.getRestaurant().getTables().get(0).info());
-        table2.setText(Restaurant.getRestaurant().getTables().get(1).info());
-        table3.setText(Restaurant.getRestaurant().getTables().get(2).info());
-        table4.setText(Restaurant.getRestaurant().getTables().get(3).info());
-        table5.setText(Restaurant.getRestaurant().getTables().get(4).info());
-        table6.setText(Restaurant.getRestaurant().getTables().get(5).info());
-        table7.setText(Restaurant.getRestaurant().getTables().get(6).info());
+        int i=0,j=0;
+        for (Table table : Restaurant.getRestaurant().getTables()) {
+            JFXButton b = new JFXButton(table.info());
+            tab.add(b);
+            GridPane.setConstraints(b,i,j/2);
+            Gridpane.getChildren().add(b);
+            if(i==0)
+                i=1;
+            else
+                i=0;
+            j++;
+        }
     }
-
 
 }
